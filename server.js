03b1070bb50072516a97d07ce26667642c30cba1
Miles Olson <mpolson64@fb.com>
2018-07-09 10:12:04 -0700

    asdf

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGODB_URI);
+
+const router = express.Router();
+
+router.get('/', () => {
+  res.json({message: 'yeet'});
+});
 
 const server = app.listen(process.env.PORT, () => {
   console.log('Listening on port ' + process.env.PORT);
